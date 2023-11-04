859703d3fecd662000d6889b122ab4b6fbb837cd
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-11-04 07:51:09 +0000

    ヘッダー/フッターの作成

---
+++ app/assets/javascripts/components/footer.js.jsx
@@ -0,0 +1,22 @@
+function Footer(currentUserId) {
+    const userLink = `/users/${currentUserId["currentUserId"]}`;
+    console.log(userLink)
+  return (
+    <div className="footer">
+      <ul>
+        <a href={userLink}>
+          <li><i className="fa-solid fa-user"></i><span>mypage</span></li>
+        </a>
+        <a href="/communities">
+          <li><i className="fa-solid fa-user-group"></i><span>communities</span></li>
+        </a>
+        <a href="/announces">
+          <li><i className="fa-solid fa-bullhorn"></i><span>announces</span></li>
+        </a>
+        <a href="/">
+          <li><i className="fa-regular fa-envelope"></i><span>content</span></li>
+        </a>
+      </ul>
+    </div>
+  );
+}
--- app/assets/javascripts/components/header.js.jsx
+++ app/assets/javascripts/components/header.js.jsx
@@ -2,7 +2,7 @@
 function Header(props) {
   return (
     <div className="header">
-      <a href="/" className="header__title">
+      <a href="/users/:id" className="header__title">
         <b className="header__title__text">NewEncounters</b>
       </a>
     </div>
