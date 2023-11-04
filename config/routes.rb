714089c20e0f0cc4a27136aad659764d2c7383c6
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-11-04 07:59:47 +0000

    routsの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :my_prs, only:[:index,:show,:new,:edit,:update,:destroy]
   resources :histories, only:[:show,:new,:edit,:update,:destroy]
   resources :communities, only:[:index,:show,:destroy,:create,:update]
-  resources :user, only:[:show,:new,:edit,:update]
+  resources :users, only:[:show,:new,:edit,:update]
   resources :announces, only:[:index,:show,:new,:edit,:update,:destroy]
   
   get 'announces/index_all'
