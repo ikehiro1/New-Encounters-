4e0afc1cbe93bf82efc87ff1b75fbc25af443a5d
ikehiro1 <136701716+ikehiro1@users.noreply.github.com>
2023-11-05 11:01:22 +0000

    オープニングアニメーション

--- app/assets/javascripts/components/users_show.js.jsx
+++ app/assets/javascripts/components/users_show.js.jsx
@@ -1,33 +1,40 @@
 function UsersShow(response) {
-    const userData = response["profileData"][0]["user_data"]
-    const userId = userData["id"]
+    const userData = response["profileData"][0]["user_data"];
+    const userId = userData["id"];
     const historyLink = `/histories/${userId}`;
     const userEditLink = `/users/${userId}/edit`;
-    if (sessionStorage.getItem("Animation")) { 
+    if (sessionStorage.getItem("Animation")) {
       console.log("アニメーション実行済み");
     } else {
-        alert("aaaa")
-      sessionStorage.setItem("Animation", "Already");
-    };
-
-     return (
-      <div className="profile">
-        <h1>-My Page-</h1>
-         
-          <div>名前</div> 
-            <p>{userData["name"]}</p>
+      setTimeout(() => {
+          sessionStorage.setItem("Animation", "Already");
+      },1500)
+    }
+  
+    return (
+      <div>
+        {!sessionStorage.getItem("Animation") && (
+          <div className="opening-animation">
+            <h1>New Entertainer</h1>
+          </div>
+        )}
+        <div className="profile">
+          <h1>-My Page-</h1>
+          <div>名前</div>
+          <p>{userData["name"]}</p>
           <div>entertainer</div>
-            <p>{userData["role"]}</p>
+          <p>{userData["role"]}</p>
           <div>生年月日</div>
-            <p>{userData["birthday"]}</p>
+          <p>{userData["birthday"]}</p>
           <div>趣味</div>
-          
           <a href={historyLink}>
             <b>My History</b>
-          </a><br/> 
-          <a href={userEditLink}> 
+          </a>
+          <br />
+          <a href={userEditLink}>
             <b>My Page Edit</b>
           </a>
+        </div>
       </div>
     );
-}
+  }
